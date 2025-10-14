cdad749620263369933ada9f4d01a78679ab0932
vista-juan-constantine <juan.constantine@vistadash.com>
2025-10-13 23:37:48 -0500

    Added the Title component

--- src/app/(event)/feeds/page.tsx
+++ src/app/(event)/feeds/page.tsx
@@ -1,12 +1,14 @@
 
 import { mockPosts } from "@/data/mockData";
-import { FeedGrid } from "@/components";
+import { FeedGrid, Title } from "@/components";
 
 export default function FeedsPage() {
   return (
     <div className="space-y-6">
       <div className="flex items-center justify-between">
-        <h2 className="text-2xl font-bold text-gray-900">Tu Feed</h2>
+        
+        <Title className="text-gray-900" title="Tu Fed" />
+
         <div className="flex space-x-2">
           <button className="px-4 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition-colors duration-200 font-medium">
             Para ti
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './feeds/FeedGrid';
 export * from './ui/sidebar/Sidebar';
+export * from './ui/title/Title';
