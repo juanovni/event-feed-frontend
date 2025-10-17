43cd55b5187064d7a152877610ca80d6ded28eac
vista-juan-constantine <juan.constantine@vistadash.com>
2025-10-17 10:54:19 -0500

    Update page.tsx

--- src/app/(event)/page.tsx
+++ src/app/(event)/page.tsx
@@ -1,18 +1,35 @@
 
-import { EventGrid, Title } from "@/components";
+import { Button, EventGrid, Title } from "@/components";
 import { mockEvents } from "@/data/mockData";
+
+type ButtonVariant = "default" | "outline" | "secondary" | "ghost";
+
+const bottonLinks: { label: string; variant: ButtonVariant }[] = [
+  {
+    label: 'Para ti',
+    variant: "default",
+  },
+  {
+    label: 'Siguiendo',
+    variant: "default",
+  }
+];
 
 export default function EventsPage() {
   return (
     <div className="space-y-4">
 
       <Title title="Eventos">
-        <button className="px-4 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition-colors duration-200 font-medium">
-          Para ti
-        </button>
-        <button className="px-4 py-2 text-gray-600 hover:bg-gray-100 rounded-full transition-colors duration-200 font-medium">
-          Siguiendo
-        </button>
+        {bottonLinks.map(item => (
+          <Button
+            key={item.label}
+            size="icon-lg"
+            variant={`${item.variant}`}
+            className="rounded-full w-30"
+          >
+            {item.label}
+          </Button>
+        ))}
       </Title>
 
       <EventGrid events={mockEvents} />
