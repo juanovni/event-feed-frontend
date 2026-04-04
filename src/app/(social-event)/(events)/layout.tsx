69979d06dabd2f13861fccad673406c74769ceec
juanovni <constantinemurillo@gmail.com>
2026-04-04 13:26:54 -0500

    Fixedd the layout

--- src/app/(social-event)/(events)/layout.tsx
+++ src/app/(social-event)/(events)/layout.tsx
@@ -10,17 +10,14 @@ export default function EventsLayout({
   return (
     <>
       {/* Contenido principal */}
-      <div className="flex w-full max-w-5xl mx-auto gap-10">
+      <div className="flex w-full md:max-w-5xl mx-auto gap-10">
         {/* Feed central */}
         <div className="w-full max-w-xl">{children}</div>
 
         {/* Columna de sugerencias */}
         <aside className="hidden lg:block w-80">
-          {/* <h2 className="px-1 py-2 font-semibold text-gray-800 mb-4">
-            Sugerencias para ti
-          </h2> */}
           <div className="sticky top-6 space-y-6">
-            <section className="">
+            <section>
               <UserSuggestions />
             </section>
           </div>
